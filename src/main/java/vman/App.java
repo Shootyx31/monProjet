aad448a0a11724c20cbe7a599a34779015a389c3
Shootyx31 <shootyx31@hotmail.fr>
2016-04-14 14:28:10 +0200

    monProjet-1 Issue #1

--- src/main/java/vman/App.java
+++ src/main/java/vman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World biatch!" );
+        System.out.println( "Hello World Issue 1 !" );
     }
 }
